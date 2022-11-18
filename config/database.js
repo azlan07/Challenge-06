b8b9ea03a8c16e31e338aeebf90af7a201b2a8d1
Azlan <azlan.nur15.07hakim@gmail.com>
2022-11-18 22:50:18 +0700

    coba2

--- config/database.js
+++ config/database.js
@@ -4,52 +4,7 @@
  */
 
 /** Destruct environment variable to get database configuration */
-// const { DB_USERNAME = 'postgres', DB_PASSWORD = 'azlan', DB_HOST = '127.0.0.1', DB_NAME = 'chapter6' } = process.env;
-
-// module.exports = {
-//   development: {
-//     username: DB_USERNAME,
-//     password: DB_PASSWORD,
-//     database: `${DB_NAME}`,
-//     host: DB_HOST,
-//     dialect: 'postgres',
-//   },
-//   test: {
-//     username: DB_USERNAME,
-//     password: DB_PASSWORD,
-//     database: `${DB_NAME}`,
-//     host: DB_HOST,
-//     dialect: 'postgres',
-//   },
-//   production: {
-//     username: DB_USERNAME,
-//     password: DB_PASSWORD,
-//     database: `${DB_NAME}`,
-//     host: DB_HOST,
-//     dialect: 'postgres',
-//   },
-// };
-
-
-//coba
-
-const { Sequelize } = require("sequelize");
-
-/** Destruct environment variable to get database configuration */
-const {
-  DB_USERNAME = "postgres",
-  DB_PASSWORD = "rAi8nENAf0do3cwOZVy6",
-  DB_HOST = "containers-us-west-126.railway.app",
-  DB_NAME = "railway",
-  DB_PORT = "6991",
-  DB_URI = "postgresql://postgres:rAi8nENAf0do3cwOZVy6@containers-us-west-126.railway.app:6991/railway"
-} = process.env;
-
-const db = new Sequelize(DB_URI,{
-  define:{
-    timestamps:false
-  }
-})
+const { DB_USERNAME = '', DB_PASSWORD = '', DB_HOST = '127.0.0.1', DB_NAME = 'chapter6' } = process.env;
 
 module.exports = {
   development: {
@@ -57,27 +12,22 @@ module.exports = {
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
-    port : DB_PORT
-    
+    dialect: 'postgres',
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
-    port : DB_PORT
-
+    dialect: 'postgres',
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
-    port : DB_PORT
-
+    dialect: 'postgres',
   },
-  db
 };
+
+
