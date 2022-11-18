bd54be1ba50be60eab5ed284ae1458eaf0577fbe
Azlan <azlan.nur15.07hakim@gmail.com>
2022-11-18 22:37:38 +0700

    vanilla

--- config/database.js
+++ config/database.js
@@ -3,22 +3,8 @@
  * @author kelompok5
  */
 
- const { Sequelize } = require("sequelize");
-
 /** Destruct environment variable to get database configuration */
-const { DB_USERNAME = "postgres",
-  DB_PASSWORD = "rAi8nENAf0do3cwOZVy6",
-  DB_HOST = "containers-us-west-21.railway.app",
-  DB_NAME = "railway",
-  DB_PORT = "6991",
-  DB_URI = "postgresql://postgres:rAi8nENAf0do3cwOZVy6@containers-us-west-126.railway.app:6991/railway"
-} = process.env;
-
-const db = new Sequelize(DB_URI,{
-  define:{
-    timestamps:false
-  }
-})
+const { DB_USERNAME = 'postgres', DB_PASSWORD = 'azlan', DB_HOST = '127.0.0.1', DB_NAME = 'chapter6' } = process.env;
 
 module.exports = {
   development: {
@@ -26,27 +12,20 @@ module.exports = {
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
-    port : DB_PORT
-    
+    dialect: 'postgres',
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
-    port : DB_PORT
-
+    dialect: 'postgres',
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
-    port : DB_PORT
-
+    dialect: 'postgres',
   },
-  db
 };
